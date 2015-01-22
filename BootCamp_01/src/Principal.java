6a580de53929cc9fa264a0887d698b40d4738cba
Roberto Sotar <rsotar@gmail.com>
2015-01-22 01:16:41 -0300

    add firuletes

--- BootCamp_01/src/Principal.java
+++ BootCamp_01/src/Principal.java
@@ -13,7 +13,9 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		
+		System.out.println("******************");
 		System.out.println("Hola Bootcamp !!!");
+		System.out.println("******************");
 
 	}
 
