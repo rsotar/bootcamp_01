ba48eab89c1fd3d15f1d7a236061fe4c24d086c4
Roberto Sotar <rsotar@gmail.com>
2015-01-22 01:02:14 -0300

    primer commit

---
+++ BootCamp_01/src/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author rober
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Bootcamp !!!");
+
+	}
+
+}
